be5584068b44493618fe8d157f00c0b701fd037f
joswa772 <itsjoswa@gmail.com>
2025-03-28 11:42:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
+

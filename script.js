ed6c07cafdb752b7fa1ffd7f8ade2908fca83db0
joswa772 <itsjoswa@gmail.com>
2025-04-03 15:15:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menuIcon = document.querySelector(".menu-icon");
-    const navLinks = document.querySelector(".nav-links");
-
-    menuIcon.addEventListener("click", () => {
-        navLinks.classList.toggle("show");
-    });
-});
-
